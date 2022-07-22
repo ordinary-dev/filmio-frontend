fa8a508212144600c4218e2df96c0cc0aa583b41
Ivan Reshetnikov <ordinarydev@protonmail.com>
2022-07-22 19:20:03 +0500

    Fixes for getting the backend address
    Changed the names of the environment variables so that they are correctly read by next js. Added a separate variable to specify the connection protocol.

--- helpers/config.ts
+++ helpers/config.ts
@@ -1,8 +1,11 @@
+const getConfig = (envVar: string|undefined, defaultValue: string) => {
+	if (envVar) return envVar
+	return defaultValue
+}
+
 export const get_backend_address = () => {
-    let backend_host = process.env.BACKEND_HOST ? process.env.BACKEND_HOST : 'filmio-backend'
-    if (!backend_host.startsWith('http')) {
-        backend_host = 'http://' + backend_host
-    }
-    const backend_port = process.env.BACKEND_PORT ? process.env.BACKEND_PORT : '8000'
-    return `${backend_host}:${backend_port}`
+    const backendScheme = getConfig(process.env.NEXT_PUBLIC_BACKEND_SCHEME, 'http')	
+    const backendHost   = getConfig(process.env.NEXT_PUBLIC_BACKEND_HOST, 'localhost')
+    const backendPort   = getConfig(process.env.NEXT_PUBLIC_BACKEND_PORT, '8000')
+    return `${backendScheme}://${backendHost}:${backendPort}`
 }
